e573e4d9e4bf83921dd9c4eb3fc1d8b757f663ca
karuna <karunabot1@gmail.com>
2024-07-09 10:11:25 +0545

    day3

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const connectToDb = require("./database/databaseConnection")
 const Blog = require("./model/blogmodel")
 const app = express()
+// required("./middleware")
+const {multer,storage}=require('./middleware/multerConfig')
+const upload =multer({storage : storage})
+
 
 connectToDb()
 
@@ -9,10 +13,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.set('view engine','ejs')
 
-app.get("/",(req,res)=>{ //() should be in order req,res while the names can be changed.
-    console.log(req)
-    res.send("<h1>this home page </h1>")  //'<h1>' is tag while '<h1>Hello World </h1>' is element
-})
+// app.get("/",(req,res)=>{ //() should be in order req,res while the names can be changed.
+//     console.log(req)
+//     res.send("<h1>this home page </h1>")  //'<h1>' is tag while '<h1>Hello World </h1>' is element
+// })
 
 app.get("/about",(req,res)=>{ 
     const name= "karuna"
@@ -29,23 +33,41 @@ app.get("/createblog",(req,res)=>{
     res.render("createblog.ejs", {create})
 })
 
-app.post("/createblog", async (req,res)=>{
+app.post("/createblog",upload.single('image'), async (req,res)=>{
     // const title = req.body.blog-title
     // const content = req.body.blog-description
     // const subtitle =req.body.blog-subtitle
     const {title, subtitle, description} = req.body
+    const filename=req.file.filename;
     console.log(title, subtitle, description)
     console.log(req.body)
 
-    await Blog.create({
+   const blog= await Blog.create({
         title, // title : title,
         subtitle, // subtitle : subtitle,
-        description // description : description
+        description, // description : description
+         image: filename
     })
+ 
+    // .render
 
-    res.send("Post hitted.")
+   res.status(201).json({
+    success:true,
+    message:"Blog created sucessfully!",
+    data:blog
+   })
 
 })
+// app.get("/home",(req,res)=>{
+//     const create = "home"
+//     res.render("home.ejs", {create})
+// })
+app.get("/",async(req,res)=>{
+    const blogs=await Blog.find();
+    res.render("home.ejs",{blogs})
+}
+)
+app.use(express.static("./storage"))
 app.listen(3000, ()=>{
     console.log("Nodejs project has started at port" + 3000)
 })
--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -2,7 +2,10 @@ const mongoose=require("mongoose")
 // import 'mangoose'
 async function connectTodb(){
 
-    await mongoose.connect("mongodb+srv://noname:anitaram@cluster0.d3qrugx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    // await mongoose.connect("mongodb+srv://noname:anitaram@cluster0.d3qrugx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect("mongodb://localhost:27017",{
+        dbName:"Karuna-Blog"
+    })
     console.log("Database connected")
 }
 module.exports=connectTodb
