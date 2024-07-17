e7e548c753d4080f34cdf738097019dff4d7b074
karuna <karunabot1@gmail.com>
2024-07-17 11:40:33 +0545

    finaltouch

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -2,11 +2,21 @@ const mongoose=require("mongoose")
 // import 'mangoose'
 async function connectTodb(){
 
-    // await mongoose.connect("mongodb+srv://noname:anitaram@cluster0.d3qrugx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    await mongoose.connect("mongodb://localhost:27017",{
-        dbName:"Karuna-Blog"
-    })
+     await mongoose.connect("mongodb+srv://noname:anitaram@cluster0.d3qrugx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    //  await mongoose.connect("mongodb://localhost:3000",{
+    //     dbName:"Karuna-Blog"
+    //  })
     console.log("Database connected")
 }
 module.exports=connectTodb
 // export default mangoose
+
+// const mongoose = require("mongoose") //cjs
+// //import 'mongoose' -- ejs
+
+// async function connectToDb(){  //function connectToDb =() =>{ 
+//     await mongoose.connect("mongodb+srv://noname:anitaram@cluster0.d3qrugx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+//     console.log("Database connected")
+// }
+
+// module.exports = connectToDb;
